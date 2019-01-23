2dbfbf6d6e6f9a71bce826de0908672d14285539
reila <pethe.annamaria@gmail.com>
2019-01-23 11:33:26 +0100

    add isOppositeColor method in Card file

--- src/com/codecool/klondike/Card.java
+++ src/com/codecool/klondike/Card.java
@@ -80,7 +80,7 @@ public class Card extends ImageView {
     public static boolean isOppositeColor(Card card1, Card card2) {
         if ((card1.getSuit() == Suit.hearts || card1.getSuit() == Suit.diamonds &&
             card2.getSuit() == Suit.clubs || card2.getSuit() == Suit.spades) &&
-            (card1.getSuit() == Suit.clubs || card2.getSuit() == Suit.spades &&
+            (card1.getSuit() == Suit.clubs || card1.getSuit() == Suit.spades &&
             card2.getSuit() == Suit.hearts || card2.getSuit() == Suit.diamonds)) {
             return true;
         } else
